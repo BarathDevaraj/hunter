4da8cff40b544936f1090baa97e8e81c94b72f4f
BarathDevaraj <52287990+BarathDevaraj@users.noreply.github.com>
2019-08-13 14:49:12 +0530

    Update a9.py

--- a9.py
+++ a9.py
@@ -1,7 +1,16 @@
 n=int(input())
 d=list(map(int,input().split()[:n]))
+count=0
 for i in range(n):
 	for j in range(i+1,n):
 		if (d[i]+d[j]==0):
 			print(d[i],end=' ')
 			print(d[j])
+			count=1
+if(count==1):
+	for i in range(n):
+		for j in range(i+1,n):
+			if (d[i]+d[j]==-1 or d[i]+d[j]==1):
+				print(d[i],end=' ')
+				print(d[j])
+				
