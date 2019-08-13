b2060ad2c4ed5428499534f9d5e87a555e7043d5
BarathDevaraj <52287990+BarathDevaraj@users.noreply.github.com>
2019-08-13 14:51:49 +0530

    Update a9.py

--- a9.py
+++ a9.py
@@ -8,9 +8,9 @@ for i in range(n):
 			print(d[j])
 			count=1
 if(count==1):
-	for i in range(n):
-		for j in range(i+1,n):
-			if (d[i]+d[j]==-1 or d[i]+d[j]==1):
-				print(d[i],end=' ')
-				print(d[j])
+	for x in range(n):
+		for y in range(x+1,n):
+			if (d[x]+d[y]==-1 or d[x]+d[y]==1):
+				print(d[x],end=' ')
+				print(d[y])
 				
